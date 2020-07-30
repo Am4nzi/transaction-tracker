8269aa9dfea3f00c934150407032f9bb26cea36f
Am4nzi <gvs.0027@gmail.com>
2020-07-30 16:49:37 +0100

    Add index component

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import TransactionsIndex from '../views/transactions/index';
 import TransactionsCreate from '../views/transactions/create';
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes: [
+    { path: '/transactions/create', component: TransactionsIndex },
     { path: '/transactions/create', component: TransactionsCreate }
   ]
 });
