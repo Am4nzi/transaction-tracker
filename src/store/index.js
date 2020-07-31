27e803b93d305b0e3ae4ee51f4cbea1b79c30c96
Am4nzi <gvs.0027@gmail.com>
2020-07-31 20:52:26 +0100

    Add more dummy transactions

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,11 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     transactions: [
-      { type: 'debit', description: 'Vue Class', amount: 4500 },
+      { type: 'debit', description: 'Eating Out', amount: 3500 },
+      { type: 'debit', description: 'Weekly City Transport', amount: 7500 },
       { type: 'credit', description: 'Payroll', amount: 100000 },
+      { type: 'debit', description: 'Airplane Tickets', amount: 38700 },
+      { type: 'credit', description: 'Freelance Project', amount: 300000 },
     ]
   },
   getters: {
